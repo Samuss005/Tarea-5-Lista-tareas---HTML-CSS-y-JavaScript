b3c15885c371fc47ae3d4c38eb3ed67e011cad10
samuele77 <samuele.moccia77@gmail.com>
2026-03-27 11:47:21 +0000

    update js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+/**Funcion que se ejecuta cuando se aprieta el boton añadir  */
 function anadirTarea() {
 
+    /* Buscamos el input en el html (el boton añadir)*/ 
     const input = document.getElementById("tarea");
+    /*Es el lugar donde se van a guardar las tareas que pongamos*/
     const lista = document.getElementById("lista");
-
+     /*Crea un div que sera la tarea nueva*/
     const tarea = document.createElement("div");
+    /*Se coge el valor que escribamos */
     tarea.innerText = input.value;
 
     lista.appendChild(tarea); /*esta parte la busque en internet*/
